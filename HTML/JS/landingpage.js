1d857d56bbd88f56d1972f161dc63d5889c0c575
Khushalshambu <kushalshambu@gmail.com>
2022-06-22 17:41:14 -0400

    day 3 of 30 days of java

---
+++ HTML/JS/landingpage.js
@@ -0,0 +1,34 @@
+var firstName = "Khushal"
+var lastName = "Shambu"
+var country = "India"
+var city = "hyd"
+var age = 21
+var isMarried = false
+var year = 2000
+console.log("hey", typeof firstName)
+console.log("hey", typeof lastName)
+console.log("hey", typeof country)
+console.log("hey", typeof city)
+console.log("hey", typeof age)
+console.log("hey", typeof isMarried)
+console.log("10" === 10)
+console.log (parseInt("9.8") === 10)
+let var1 = "python"
+let var2 = "jargon"
+console.log(var1.length === var2.length)
+
+let b = parseInt(prompt("enter b"))
+let h = parseInt(prompt("enter h"))
+console.log(0.5*b*h)
+let a = parseInt(prompt("enter b"))
+let c = parseInt(prompt("enter h"))
+console.log(a*b)
+console.log(2*(a+b))
+let arr1 = [2,2]
+let arr2 = [6,10]
+console.log("the slop is", (arr2[1]-arr1[1])/(arr2[0]-arr1[0]))
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+firstName.length > lastName.length
+  ? console.log(firstName + " " + "is longer than " + " "+ lastName)
+  : console.log(firstName + "is shorter than" + lastName)
